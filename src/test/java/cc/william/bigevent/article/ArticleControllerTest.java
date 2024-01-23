f1cf5622ff3c1f83ffbf0a19aae946a36231e016
William Wu <william.wu@ibm.com>
2024-01-23 17:11:57 +0800

    add Test case

---
+++ src/test/java/cc/william/bigevent/article/ArticleControllerTest.java
@@ -0,0 +1,25 @@
+/*
+ * ===========================================================================
+ * IBM Confidential
+ * iX Source Materials
+ * (C) Copyright IBM Corp. 2024.
+ * ===========================================================================
+ */
+package cc.william.bigevent.article;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * <p>
+ * description
+ * </p>
+ *
+ * @author William Wu
+ * @version 1.0, 2024-01-23
+ * @see
+ * @since
+ */
+
+@SpringBootTest
+public class ArticleControllerTest {
+}
